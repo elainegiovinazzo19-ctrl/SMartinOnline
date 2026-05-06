21a2043b8e5efbfcbd8f2353b1846be606d0da91
elainegiovinazzo19-ctrl <elainegiovinazzo19@gmail.com>
2026-05-06 10:45:53 -0300

    agrego estructura proyecto y entidad Usuario

---
+++ SMartinOnline.Entidades/SMartinOnline.Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMartinOnline.Entidades
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasena { get; set; }
+        public string Rol { get; set; }
+        public bool Activo { get; set; }
+        public int IntentosLogin { get; set; }
+    }
+}
