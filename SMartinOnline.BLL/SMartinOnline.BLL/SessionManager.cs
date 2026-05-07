a40d24461e0224ff2c347b6cd81c3167ba35683e
elainegiovinazzo19-ctrl <elainegiovinazzo19@gmail.com>
2026-05-07 19:18:52 -0300

    agrego SessionManager y DAL de usuario

---
+++ SMartinOnline.BLL/SMartinOnline.BLL/SessionManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMartinOnline.Entidades;
+
+namespace SMartinOnline.BLL
+{
+    public class SessionManager
+    {
+        private static SessionManager _instancia;
+        private Usuario _usuarioActual;
+
+        private SessionManager() { }
+
+        public static SessionManager Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                    _instancia = new SessionManager();
+                return _instancia;
+            }
+        }
+
+        public Usuario UsuarioActual
+        {
+            get { return _usuarioActual; }
+        }
+
+        public void IniciarSesion(Usuario usuario)
+        {
+            _usuarioActual = usuario;
+        }
+
+        public void CerrarSesion()
+        {
+            _usuarioActual = null;
+        }
+
+        public bool HaySesionActiva()
+        {
+            return _usuarioActual != null;
+        }
+    }
+}
---
+++ SMartinOnline.DAL/UsuarioDAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMartinOnline.Entidades;
+
+namespace SMartinOnline.DAL
+{
+    public class UsuarioDAL
+    {
+        private string connectionString = "Data Source=.;Initial Catalog=SMartinOnline;Integrated Security=True";
+
+        public Usuario ObtenerPorUsuario(string nombreUsuario)
+        {
+            Usuario usuario = null;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Usuario WHERE NombreUsuario = @NombreUsuario";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@NombreUsuario", nombreUsuario);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    usuario = new Usuario();
+                    usuario.IdUsuario = (int)reader["IdUsuario"];
+                    usuario.NombreUsuario = reader["NombreUsuario"].ToString();
+                    usuario.Contrasena = reader["Contrasena"].ToString();
+                    usuario.Rol = reader["Rol"].ToString();
+                    usuario.Activo = (bool)reader["Activo"];
+                    usuario.IntentosLogin = (int)reader["IntentosLogin"];
+                }
+            }
+            return usuario;
+        }
+
+        public void ActualizarIntentosLogin(int idUsuario, int intentos)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Usuario SET IntentosLogin = @Intentos WHERE IdUsuario = @IdUsuario";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Intentos", intentos);
+                cmd.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void BloquearUsuario(int idUsuario)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Usuario SET Activo = 0 WHERE IdUsuario = @IdUsuario";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
